9664ffaf9a091e8a07987dc095eedd1435ab27ec
kuro171492-cyber <kuro171492@gmail.com>
2026-06-08 15:28:52 +0300

    fix: Correct template card structure in optimized renderer - single row header with chip, name, time + buttons row

--- ui_components.js
+++ ui_components.js
@@ -238,18 +238,19 @@
                 card.className = 'template-card';
                 card.dataset.templateId = t.id;
                 card.innerHTML = `
-                    <div class="template-menu template-menu-top">
+                    <div class="template-top-row">
+                        <span class="template-chip">${t.weekday}</span>
+                        <input type="text" class="template-name-input" value="${t.name}" onkeydown="handleTemplateNameKey(event, '${t.id}')" onblur="saveTemplateName(this, '${t.id}')">
+                        <span class="template-time">${t.totalTime || '00:00'}</span>
+                    </div>
+                    <div class="template-exercises-preview hidden mt-2" data-loaded="0"></div>
+                    <div class="template-menu">
                         <button onclick="applyTemplateById('${t.id}')" class="template-menu-btn primary" title="Применить">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><polygon points="5 3 19 12 5 21 5 3"/></svg>
                         </button>
                         <button onclick="toggleTemplateExercises(this)" class="template-menu-btn" title="Показать упражнения">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/><circle cx="12" cy="12" r="3"/></svg>
                         </button>
-                    </div>
-                    <input type="text" class="template-name-input" value="${t.name}" onkeydown="handleTemplateNameKey(event, '${t.id}')" onblur="saveTemplateName(this, '${t.id}')">
-                    <span class="text-[10px] text-slate-500 font-bold uppercase">${t.totalTime || '00:00'}</span>
-                    <div class="template-exercises-preview hidden mt-2" data-loaded="0"></div>
-                    <div class="template-menu">
                         <button onclick="focusTemplateName('${t.id}')" class="template-menu-btn" title="Переименовать">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
                         </button>
